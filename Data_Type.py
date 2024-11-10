a09e2d8217ae99c15213043419de9c65930ac05e
Rafi <latifurrafi@gmail.com>
2024-11-10 10:26:59 +0600

    Problem

---
+++ Data_Type.py
@@ -0,0 +1,29 @@
+# list = ["Dhaka", "Dublin", "Silicon Vally"]
+# list = ["200", "Berlin", "19.99"]
+# list = ["Dhaka", "Dublin", "Dhaka"]
+
+# print(list[2])
+
+
+
+# tuple = ("a","b","c")
+# tuple = (1,"3","f")
+# print(tuple)
+
+# range data type
+
+# number = range(1,10)
+# print(*number)
+# print("\n")
+# number = range(0,16,2)
+# print(*number)
+
+# Dict Tyype
+
+person = {
+    'first name' : 'Latifur',
+    'last name'  : "Rafi",
+    'age' : 24,
+    "isBangladeshi" : True
+}
+print(*person.values())
